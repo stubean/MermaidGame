e587c00e48be21f7e16905c940a0c92d35798e1d
Yuan Wei <raynorwei@gmail.com>
2018-07-19 19:21:40 -0400

    BubbleUpdate (Not Finished)

--- Mermaid Game/Assets/Bubble.cs
+++ Mermaid Game/Assets/Bubble.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Bubble : MonoBehaviour {
 
+    private float upWardSpeed = 1f;
+
 	// Use this for initialization
 	void Start () {
         this.transform.parent = null;
@@ -11,6 +13,18 @@ public class Bubble : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        this.transform.Translate(Vector3.up * Time.deltaTime);
+        this.transform.Translate(Vector3.up * upWardSpeed * Time.deltaTime);
 	}
+
+    private void DestroyItSelf() {
+        Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Kid") {
+            upWardSpeed = 0f;
+            Invoke("DestroyItSelf", 4);
+        }
+    }
 }
