e8ab3883ab4bab04bfec0c1a6a7e0f86c6de299d
Yuan Wei <raynorwei@gmail.com>
2018-08-01 18:32:23 -0400

    Using Scripted Water

---
+++ Mermaid Game/Assets/DrawSize.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawSize : MonoBehaviour {
+
+    WaterLine waterLine;
+
+    void OnDrawGizmos()
+    {
+        waterLine = gameObject.GetComponent<WaterLine>();
+        float waterHeight = waterLine.Height;
+        float waterWidth = waterLine.Width;
+
+        Vector2 drawSize = new Vector2(waterWidth, waterHeight);
+        Vector2 drawPosition = new Vector2(transform.position.x, transform.position.y - waterHeight/2);
+
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireCube(drawPosition, drawSize);
+    }
+}
